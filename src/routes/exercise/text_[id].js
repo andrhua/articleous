155b3889b3ff39fafabf74f593e2ec651eac13a2
andrhua <andrhua@pm.me>
2020-04-11 17:07:34 +0300

    cleanup

--- src/routes/exercise/text_[id].js
+++
@@ -1,9 +0,0 @@
-import { getText } from '../../utils/db.js';
-
-
-export async function get(req, res, next) {
-	const { id } = req.params;
-	const text = await getText(id);
-	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify(text));
-}
