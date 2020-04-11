07a81394b5db44199f22caa9455bc7f6d5f89d18
andrhua <andrhua@pm.me>
2020-04-11 17:04:00 +0300

    rename to smth more suitable

--- src/stores/trainer.js
+++ src/stores/trainer.js
@@ -2,7 +2,7 @@ import { writable, readable } from 'svelte/store';
 
 export const answer = writable();
 export const exerciseId = writable(-1);
-export const State = readable({'edit': 0, 'review': 1, 'stats': 2});
+export const State = readable({'edit': 0, 'review': 1, 'finish': 2});
 export const filled = writable(false);
 
 function createFocusId() {
