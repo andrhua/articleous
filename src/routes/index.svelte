32fe69c9460d2ec5d426e509aeae0bfaab4ce8f6
andrhua <andrhua@pm.me>
2020-04-14 14:17:30 +0300

    replace flex with grid and cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,6 @@
 <script>
 	import { goto } from '@sapper/app';
 
-	function handleClick(event) {
-		goto('/auth/github');
-	}
-
 	const name = 'articleous';	
 </script>
 
@@ -12,27 +8,28 @@
 	main {
 		width: 100%;
 		height: 100%;
-		display: flex;
-	}
-
-	#left, #right {
-		transition: all 0.2s;
+		display: grid;
+		grid-template-rows: 1fr;
+		grid-template-columns: 0.7fr 0.3fr;
 	}
 
 	#left {
-		display: flex;
-		justify-content: center;
+		width: 100%;
+		height: 100%;
+		display: grid;
+		grid-template-rows: 0.7fr 0.3fr;
+		grid-template-columns: 1fr;
+		justify-items: center;
 		align-items: center;
-		flex: 4;
 		color: var(--near-black);
 	}
 
 	#right {
-		background-color: var(--ultramarine-blue);
-		flex: 2;
+		display: flex;
 		justify-content: center;
 		align-items: flex-end;
-		display: flex;
+		padding-bottom: 7em;
+		background-color: var(--ultramarine-blue);
 	}
 
 	.circle {
@@ -48,61 +45,27 @@
 		border: 0;
 	}
 
-	#features {
-		font: 1.7em 'Playfair Display';
-		position: absolute;
-		top: 100px;
-		right: 120px;
-		color: var(--near-black);
-		width: 300px;
-	}
-
-	#left-wrap {
-		flex-direction: column;
-		justify-content: flex-start;
-		align-items: center;
-		width: 50%;
-	}
-
 	h1 {
 		text-transform: uppercase;
 		font-size: 5em;
 		font-family: 'Playfair Display';
-		padding-bottom: 1em;
 	}
 
-	#ezekiel {
-		font: 1.2em 'Oxygen Light';
-	}	
-
-	#provider {
+	.provider {
 		display: flex;
-		flex-direction: column;
-		justify-content: end;
+		justify-content: center;
 		align-items: center;
-		font: 0.95em 'JetBrains Mono';
-		border-radius: 16px;
 		background-color: var(--off-white);
-		--size: 100px;
+		--size: 50px;
 		width: var(--size);
 		height: var(--size);
-		margin-bottom: 4em;
-		transition: all 0.2s;
-	}
-
-	#provider:hover {
-		transition: all 0.2s;
-		box-shadow: 1px 1px 6px 1px var(--mint);
-	}
-
-	#provider:active {
-		transition: all 0.2s;
-		transform: scale(0.98);
 	}
 
 	.sir { 
-		width: 100%;
-		height: 100%;
+		position: absolute;
+		top: 100px;
+		right: 120px;
+		width: 300px;
 		margin: 8px;
 		opacity: 0%;
 		transition: opacity 1s;
@@ -118,7 +81,6 @@
 		height: 48px;
 		margin: 8px;
 	}
-
 </style>
 
 <svelte:head>
@@ -126,21 +88,15 @@
 </svelte:head>
 
 
+<div class='circle'></div>
+<img class='sir' src='sergay.jpg', alt='backpack dude'>
 <main>
-	<div class='circle'></div>
-	<div id='features'><img class='sir' src='sergay.jpg', alt='backpack dude'></div>
-
 	<div id='left'>
-		<div id='left-wrap'>
-			<h1>{name}</h1>
-			<div id='ezekiel'>
-				я тебя люблю
-			</div>
-		</div>
+		<h1>{name}</h1>
+		<div id='ezekiel'>я тебя люблю</div>
 	</div>
 	<div id='right'>
-		<div id='provider' on:click={handleClick} >
-			Log in
+		<div class='floating-button provider' on:click={() => goto('/auth/github')} >
 			<img src='icons/github.svg' alt='Github logo' class='github'>
 		</div>
 	</div>
